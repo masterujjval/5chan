819fb2cb20d198ee98d496f4c0b7671339014f16
masterujjval <guptarickey3@gmail.com>
2025-06-21 18:21:46 +0530

    installer.sh added

---
+++ installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo cp client.py /usr/local/bin
+sudo chmod +x /usr/local/bin/client.py
+sudo mv /usr/local/bin/client.py /usr/local/bin/5chan
+echo "5chan client installed successfully. You can run it using the command '5chan'... Be an anon!"
